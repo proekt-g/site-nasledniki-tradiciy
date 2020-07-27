1d396819753ef9bcaa01d53574bb8d8280e1e0dc
Armen <armen.armen.armen95@gmail.com>
2020-07-27 19:19:28 +0300

    first show

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,18 @@
-window.addEventListener('load', function() {});
+window.addEventListener('load', function() {
+    function clickFilter() {
+        $('.part-filter-content__main').not($(this)).removeClass('active');
+        $('.part-filter-content__submain').not($(this).next()).slideUp(300);
+        $(this).toggleClass('active').next().slideToggle(300);
+    }
+    document.querySelectorAll('.part-filter-content__main').forEach(function(item) {
+        item.addEventListener('click', clickFilter);
+    });
+    document.querySelectorAll('.part-filter-content__submain').forEach(function(item) {
+        item.addEventListener('click', function() {
+            let buffText = this.closest('.part-filter-content__item').querySelector('.part-filter-content__main').textContent;
+            this.closest('.part-filter-content__item').querySelector('.part-filter-content__main').textContent = this.textContent
+            this.textContent = buffText;
+            clickFilter();
+        });
+    })
+});
