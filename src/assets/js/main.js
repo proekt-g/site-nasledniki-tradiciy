34f376bc6e683339e092c708472774b9c1dbcb56
Armen <armen.armen.armen95@gmail.com>
2020-07-28 16:52:51 +0300

    add one page + mobail one page

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,18 +1,53 @@
+function checkWidth() {
+    if (window.innerWidth <= 768) {
+        document.querySelector('.header-menu__desktop').insertAdjacentElement('beforeend', document.querySelector('.header__log'));
+    }
+    if (window.innerWidth >= 768) {
+        document.querySelector('.header-container').insertAdjacentElement('beforeend', document.querySelector('.header__log'));
+    }
+}
 window.addEventListener('load', function() {
+    checkWidth();
+
     function clickFilter() {
         $('.part-filter-content__main').not($(this)).removeClass('active');
         $('.part-filter-content__submain').not($(this).next()).slideUp(300);
+        $('.program .filter-content__main').not($(this)).removeClass('active');
+        $('.program .filter-content__submain').not($(this).next()).slideUp(300);
         $(this).toggleClass('active').next().slideToggle(300);
     }
+
+    function clickSubmainItem() {
+        let buffText;
+        if (this.closest('.part-filter-content__item') !== null) {
+            buffText = this.closest('.part-filter-content__item').querySelector('.part-filter-content__main').textContent;
+            this.closest('.part-filter-content__item').querySelector('input').value = this.textContent;
+            this.closest('.part-filter-content__item').querySelector('.part-filter-content__main').textContent = this.textContent;
+        } else {
+            buffText = this.closest('.program .filter-content__item').querySelector('.program .filter-content__main').textContent;
+            this.closest('.program .filter-content__item').querySelector('input').value = this.textContent;
+            this.closest('.program .filter-content__item').querySelector('.program .filter-content__main').textContent = this.textContent;
+        }
+        this.textContent = buffText;
+        clickFilter();
+    }
+
+    function clickBurger() {
+        this.closest('.header-menu').classList.toggle('menu_state_open');
+        this.closest('.header-menu').querySelector('.header-menu__desktop').classList.toggle('open');
+    }
+    document.querySelectorAll('.program .filter-content__main').forEach(function(item) {
+        item.addEventListener('click', clickFilter);
+    });
     document.querySelectorAll('.part-filter-content__main').forEach(function(item) {
         item.addEventListener('click', clickFilter);
     });
-    document.querySelectorAll('.part-filter-content__submain').forEach(function(item) {
-        item.addEventListener('click', function() {
-            let buffText = this.closest('.part-filter-content__item').querySelector('.part-filter-content__main').textContent;
-            this.closest('.part-filter-content__item').querySelector('.part-filter-content__main').textContent = this.textContent
-            this.textContent = buffText;
-            clickFilter();
-        });
-    })
+    document.querySelectorAll('.part-filter-content__submain li').forEach(function(item) {
+        item.addEventListener('click', clickSubmainItem);
+    });
+    document.querySelectorAll('.program .filter-content__submain li').forEach(function(item) {
+        item.addEventListener('click', clickSubmainItem);
+    });
+    document.querySelector('.header-menu__burger').addEventListener('click', clickBurger);
 });
+window.addEventListener('resize', checkWidth);
